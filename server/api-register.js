14e45363fdaf6ac0aa7d3ff1e8bd93daf63378b5
fantalec <mikaelfant@gmail.com>
2023-12-07 09:26:13 +0100

    resolved conflict in apiregister

--- server/api-register.js
+++ server/api-register.js
@@ -6,15 +6,9 @@ import users from "./apis/users.js"
 import events_sorted from "./apis/createview-events.js"
 import clubs from "./apis/clubs.js"
 import getclub from "./apis/getclub.js"
-<<<<<<< HEAD
 import getParrotEvents from "./apis/getParrotEvents.js";
 import getNotDeadYetEvents from "./apis/getNotDeadYetEvents.js";
 import getAkzeptoEvents from "./apis/getAkzeptoEvents.js";
-=======
-import getParrotEvents from "./apis/getParrotEvents.js"
-import getAkzeptoEvents from "./apis/getAkzeptoEvents.js"
-import getNotDeadYetEvents from "./apis/getNotDeadYetEvents.js"
->>>>>>> dev
 import gala from "./apis/gala.js"
 
 
@@ -27,15 +21,7 @@ export default function (server, db) {
   clubs(server, db)
   getclub(server, db)
   getParrotEvents(server, db)
-  getAkzeptoEvents(server, db)
   getNotDeadYetEvents(server, db)
-<<<<<<< HEAD
   getAkzeptoEvents(server, db)
   gala(server, db)
-
-
 }
-=======
-  gala(server, db)
-}
->>>>>>> dev
