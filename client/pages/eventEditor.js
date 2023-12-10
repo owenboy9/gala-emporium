7b3637d016eead9eca7e69828fdf562e28c78be2
axelmartini <erik.cronqvist@gmail.com>
2023-12-10 09:15:37 +0100

    Adding edit button to backend page

--- client/pages/eventEditor.js
+++ client/pages/eventEditor.js
@@ -1,5 +1,7 @@
+let events
+
 export default async function (userId) {
-  let events = await getEventData(userId)
+  events = await getEventData(userId)
   
   console.log(userId, events)
   let clubname = events[0].clubname
@@ -9,17 +11,14 @@ export default async function (userId) {
       <h2>Edit, add or delete your events</h2>
       <p>You are currently logged in as club admin for ${clubname}
 
-      <div>${showEvents(events)}</div
+      <div>${showEvents(events)}</div>
 
     </div>
   `
 
   return html
-
-
-
-
 } 
+ 
 
 async function getEventData(userId) {
   let response = await fetch(`/api/eventEditor/${userId}`)
@@ -29,14 +28,97 @@ async function getEventData(userId) {
 }
 
 function showEvents(events) {
-  let html = ''
+  let html = ``
+  let index = 0
   for (let event of events) {
+      
     html += `
-      <div class="eventInfo">
+
+     <div class="eventEditWrapper"> 
+      <div class="eventEditItem">
+        ${fixTime(event)}
         <h2>${event.headline}</h2>
-        <p>${event.description_short}</p>
       </div>
+      <div class="eventEditButtons">
+        <button class="eventEditButton" onclick="openEventToEdit(${index})">Edit</button>
+      </div>
+     </div> 
+
+     <section id="event${event.id}"></section>
+
     `
+    index++
   }
   return html
 }
+
+function fixTime(event) {
+  let html = ``
+  let jsstarttime = new Date(event.start_time)
+  let weekday = jsstarttime.getUTCDay()
+  let date = jsstarttime.getUTCDate()
+  let month = jsstarttime.getUTCMonth()
+  let year = jsstarttime.getUTCFullYear()
+  let startHour = padZero(jsstarttime.getUTCHours())
+  let startMinute = padZero(jsstarttime.getUTCMinutes())
+
+  html += `
+    <div class="eventEditDate">
+      <div class="eventEditYear">${getDayName(weekday)}</div>
+      <div class="eventEditDay">${date}</div>
+      <div class="eventEditMonth">${getMonthName(month)}</div>
+      <div class="eventEditYear">${year}</div>
+    </div>
+  `
+  return html
+
+}
+
+function padZero(value) {
+  return value < 10 ? `0${value}` : `${value}`
+}
+
+function getMonthName(month) {
+  console.log(month)
+  let index = month
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  return months[index]
+}
+
+function getDayName(weekday) {
+  let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  return days[weekday]
+}
+
+///// edit event
+
+function openEventToEdit(index) {
+  
+  console.log(index)
+  let event = events[index]
+  console.log(event)
+  console.log(event.id)
+  console.log('open event to edit', event.id)
+  let html = ` 
+    <p>This is a test</p>
+  
+  `
+  let elementId = `#event${event.id}`
+  console.log(elementId)
+  $(elementId).html(html)
+
+ /*  try {
+    const event = JSON.parse(eventObject);
+    console.log(event);
+    console.log('open event to edit', event.id);
+    let html = `<p>This is a test</p>`;
+    let elementId = `#event${event.id}`;
+    $(elementId).html(html);
+  } catch (error) {
+    console.error('Error parsing eventObject:', error);
+  } */
+
+
+}
+
+window.openEventToEdit = openEventToEdit
